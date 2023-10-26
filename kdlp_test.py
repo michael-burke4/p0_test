f1f46bc6d6ad3b03569df7345ea6775dcd746fc0
Michael Burke <michael_burke@student.uml.edu>
2023-10-26 05:01:03 -0400

    implement semi-autonomous grading system
    the grader has to apply patches themselves to the testing dir.
    We don't want to super strictly enforce that each level is done by
    a single patch, all done perfectly sequentially.

--- kdlp_test.py
+++ kdlp_test.py
@@ -5,6 +5,7 @@ import pty
 import select
 import subprocess
 import git
+import shutil
 
 # this function is courtesy of
 # https://stackoverflow.com/a/52954716
@@ -54,28 +55,71 @@ def tty_capture(cmd, bytes_input, output_bytes=512):
 def run_test(test):
     return tty_capture(test['args'], bytes(test['input'], 'UTF-8'))
 
+def clean_cur_dir():
+    for file in os.scandir():
+        if file.is_file():
+            os.remove(file)
+        else:
+            shutil.rmtree(file)
+
+def check_repo_exists(directory):
+    inpt = 0
+    if os.listdir() != []:
+        while inpt != 'y' and inpt != 'n':
+            inpt = input("WARNING: %s is not empty. Remove all files and directories in %s? [y/n]: " % (directory, directory))
+        if inpt == 'y':
+            clean_cur_dir()
+
+
+
 def main():
     file = open('tests.json')
     js = json.load(file)
-    patch_dir = js['meta']['patches_dir']
-    files = os.listdir(patch_dir)
-    files.sort()
-    patches = [patch_dir + x for x in files if (('.patch' in x or '.eml' in x) and 'cover-letter' not in x)]
-    os.chdir(js['meta']['working_dir'])
+    working_dir = js['meta']['working_dir']
+    os.chdir(working_dir)
+
+    check_repo_exists(working_dir)
+
     repo = git.Repo.init('.')
-    level_no = 1
-    for patch, level in zip(patches, js['levels']):
-        repo.git.execute(['git', 'am', patch])
-        test_no = 1
+
+
+    level_no = 0
+    for level in js['levels']:
+        while True:
+            inp = input("Apply patches until level %d is complete. `h`elp, `c`ontinue, `a`bort, or `e`xit: " % level_no)
+            if inp == 'a':
+                return
+            elif inp == 'e':
+                clean_cur_dir()
+                return
+            elif inp == 'h':
+                print("CONTINUE: Patches for the current level have been applied, proceed to run the tests for that level.")
+                print("ABORT: Exit the program, perform no further tests, Will not delete any files in the testing directory.")
+                print("EXIT: Exit the program, perform no further tests, and completely clean the testing directory.")
+                break
+            elif inp == 'c':
+                break
+
+        test_no = 0
+        passed = 0
         for test in level:
             out, err = run_test(test)
             if out == bytes(test['expected'], 'UTF-8'):
                 print('Level', level_no, 'Test', test_no, 'PASSED')
+                passed += 1
             else:
                 print('Level', level_no, 'Test', test_no, 'FAILED: got', out, 'expected', bytes(test['expected'], 'UTF-8'))
             test_no += 1
         level_no += 1
+        print("LEVEL %d %d/%d PASSED" % (level_no, passed, test_no))
 
+
+    done = 0
+    while done != 'y' and done != 'n':
+        done = input("Testing finished. Remove all files and directories in %s? [y/n]: " % (working_dir))
+    if done == 'y':
+        clean_cur_dir()
+    return
 
 if __name__ == '__main__':
     main()
